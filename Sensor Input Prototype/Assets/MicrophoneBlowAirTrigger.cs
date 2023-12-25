862e21ce7fc750671671b09ba5735de153a4c839
Laptop\Peder <pzaric15@student.aau.dk>
2023-12-25 22:49:02 +0100

    Microphone Input version one FFT  implemented.

---
+++ Sensor Input Prototype/Assets/MicrophoneBlowAirTrigger.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SensorInputPrototype.MixinInterfaces;
+using UnityEngine.InputSystem.Controls;
+using Unity.VisualScripting;
+
+public class MicrophoneBlowAirTrigger : MonoBehaviour, MMicrophoneFifoAmp
+{
+    private UniversalPanel universalPanel;
+    private AudioSource audioSource;
+    public bool canTransition = false;
+    private AudioClip clip;
+
+    void Awake()
+    {
+        if(audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+
+        }
+        
+
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+  
+        universalPanel = GetComponent<UniversalPanel>();
+        this.MicrophonoeFifoAmpInitializer(audioSource, 8, 512, 2);
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (universalPanel != GlobalReferenceManager.GetCurrentUniversalPanel())
+        {
+            if (Microphone.IsRecording(Microphone.devices[0]))
+            {
+                Microphone.End(Microphone.devices[0]);
+            }
+            this.BufferFlush();
+            canTransition = false;
+            return; 
+        
+        }
+
+        
+
+        if (Microphone.IsRecording(Microphone.devices[0]))
+        {
+
+        }
+        else
+        {
+
+            clip = Microphone.Start(Microphone.devices[0], false, 1, 48000);
+                     
+
+            
+        
+        }
+        if (clip != null)
+        {
+            if (clip.length == 1f)
+            { 
+                this.BufferUpdate(clip, 0);
+                clip = null;
+            }
+        }
+
+        if (universalPanel.transitionType == 5)
+        {
+            
+            float lowerShelf = 0;
+            float upperShelf = 0;
+            int threshold = this.GetAvgQueueFrequencyDistribution().Length - Mathf.RoundToInt(this.GetAvgQueueFrequencyDistribution().Length / 6);
+            Debug.Log("1: " + this.GetAvgQueueFrequencyDistribution()[0] +" ,2: " + this.GetAvgQueueFrequencyDistribution()[1] + " ,3: " + this.GetAvgQueueFrequencyDistribution()[2] + " ,4: " + this.GetAvgQueueFrequencyDistribution()[3] + " ,5: " + this.GetAvgQueueFrequencyDistribution()[4] + " ,6: " + this.GetAvgQueueFrequencyDistribution()[5] + " ,7: " + this.GetAvgQueueFrequencyDistribution()[6] + " ,8: " + this.GetAvgQueueFrequencyDistribution()[7] + " ,Amplitude: " + this.GetAvgQueueAmplitude());
+
+            for (int i = 0; i < this.GetAvgQueueFrequencyDistribution().Length;i++)
+            {
+                if (i < this.GetAvgQueueFrequencyDistribution().Length - threshold)
+                {
+                    lowerShelf += this.GetAvgQueueFrequencyDistribution()[i];
+                    
+                }
+                else
+                {
+                    upperShelf += this.GetAvgQueueFrequencyDistribution()[i];
+                }
+
+            }
+            lowerShelf /= (this.GetAvgQueueFrequencyDistribution().Length - threshold);
+            upperShelf /= threshold;
+            bool isAirInput = (lowerShelf > upperShelf);
+            Debug.Log("LS: "+lowerShelf + " ,US: " + upperShelf + " isAirInput: " + isAirInput);
+            if (this.GetAvgQueueAmplitude()*10 > 0.6f && isAirInput)
+            {
+                canTransition = true;
+            }
+        }
+
+
+
+
+
+        if (canTransition)
+        {
+            universalPanel.TriggerTransition();
+            canTransition = false;
+        }
+
+    }
+}
--- Sensor Input Prototype/Assets/MicrophoneFifoAmp.cs
+++ Sensor Input Prototype/Assets/MicrophoneFifoAmp.cs
@@ -1,9 +1,10 @@
-using System.Collections;
+
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using SensorInputPrototype.MixinInterfaces;
-using Unity.VisualScripting;
+
+using System;
 
 public static class MicrophoneFifoAmp
 {
@@ -11,26 +12,214 @@ public static class MicrophoneFifoAmp
 
 
     private static ConditionalWeakTable<MMicrophoneFifoAmp, Fields> table;
-
+    
     static MicrophoneFifoAmp()
     {
         table = new ConditionalWeakTable<MMicrophoneFifoAmp, Fields>();
     }
     private sealed class Fields : MonoBehaviour, MTransition
     {
-        internal Queue<float> samplesQueue = new Queue<float>();
-    }
+        internal Queue<AudioClip> samplesQueue = new Queue<AudioClip>();
+        internal float currentAccretedAudioSampleVolume = 0.0f;
+        internal AudioSource audioSource;
+        internal int NumberOfFrequencyBands = 32;
+        internal int samplingRate;
+        internal float[] frequencyBands;
+        internal float[] bufferedSamples;
+        internal int bufferSize;
 
-    public static void Enqueue(this MMicrophoneFifoAmp map, float soundSample)
+    }
+    public static void MicrophonoeFifoAmpInitializer(this MMicrophoneFifoAmp map, AudioSource audioSource, int freqBandNums, int samplingRate, int bufferSize)
     {
+        if (Mathf.IsPowerOfTwo(freqBandNums) == false)
+        {
+            throw new ArgumentException(
+           "The Number of Frequency Bands MUST be a power of 2", nameof(freqBandNums));
+        }
+        if (Mathf.IsPowerOfTwo(samplingRate) == false)
+        {
+            throw new ArgumentException(
+           "Sampling rate MUST be a power of 2", nameof(samplingRate));
+        }
+        if (samplingRate > 8192 || samplingRate < 64)
+        {
+            throw new ArgumentException(
+           "Sampling rate must be between 64 and 8192", nameof(samplingRate));
+        }
+
+        table.GetOrCreateValue(map).audioSource = audioSource;
+        table.GetOrCreateValue(map).NumberOfFrequencyBands = freqBandNums;
+        table.GetOrCreateValue(map).frequencyBands = new float[freqBandNums];
+        table.GetOrCreateValue(map).samplingRate = samplingRate;
+        table.GetOrCreateValue(map).bufferedSamples = new float[samplingRate];
+        table.GetOrCreateValue(map).bufferSize = bufferSize;
+    }
+    public static void Enqueue(this MMicrophoneFifoAmp map, AudioClip soundSample,int position)
+    {
+        //float[] samples = new float[soundSample.samples];
+        //soundSample.GetData(samples, position);
+        //AudioClip audioClip = AudioClip.Create("tempclip", samples.Length, soundSample.channels, soundSample.frequency, false);
         table.GetOrCreateValue(map).samplesQueue.Enqueue(soundSample);
 
-        
+
     }
+    public static void BufferUpdate(this MMicrophoneFifoAmp map, AudioClip soundSample, int position)
+    {
+        Queue<AudioClip> audioClips = new(table.GetOrCreateValue(map).samplesQueue);
+        if (audioClips.Count < table.GetOrCreateValue(map).bufferSize)
+        {
+            // just enqueue but dont analyse yet.
+            Enqueue(map, soundSample, position);
+
+        }
+        else
+        {
+
+
+            float[] spectrumData = new float[table.GetOrCreateValue(map).samplingRate];
+            //float[] perRateSpectrumData = new float[table.GetOrCreateValue(map).samplingRate];
+            for (int i = 0; i < table.GetOrCreateValue(map).samplesQueue.Count; i++)
+            {
+                table.GetOrCreateValue(map).audioSource.clip = audioClips.Dequeue();
+                table.GetOrCreateValue(map).audioSource.GetSpectrumData(spectrumData, 0, FFTWindow.Hanning);
+
+                for (int j = 0; j < spectrumData.Length; j++)
+                { table.GetOrCreateValue(map).bufferedSamples[j] += spectrumData[j]; }
+
+
+            }
+
+
+            UpdateAvgLoudness(map);
+            MakeFrequencyBands(map, table.GetOrCreateValue(map).bufferedSamples);
+            
+            Enqueue(map, soundSample, position);
+
+        }
+        if(table.GetOrCreateValue(map).samplesQueue.Count > table.GetOrCreateValue(map).bufferSize)
+        { Dequeue(map); }
+
+
+
+
+
+
+
+
+
+    }
+
+    private static void MakeFrequencyBands(MMicrophoneFifoAmp map, float[] _samples)
+    {
+        int count = 0;
+        int bandnumbers = table.GetOrCreateValue(map).NumberOfFrequencyBands;
+
+        for (int i = 0; i < bandnumbers; i++)
+        {
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+            float avg = 0;
+
+
+            if (i == bandnumbers - 1)
+            {
+                sampleCount += 2;
+            }
+            for (int j = 0; j < sampleCount; j++)
+            {
+                avg += _samples[count] * (count + 1);
+                count++;
+            }
+            avg /= count;
+
+            table.GetOrCreateValue(map).frequencyBands[i] = avg * 10;
+
+        }
+
+
+
+
+    }
+
+
 
     public static void Dequeue(this MMicrophoneFifoAmp map)
     {
         table.GetOrCreateValue(map).samplesQueue.Dequeue();
+        
+
+    }
+    public static void UpdateAvgLoudness(MMicrophoneFifoAmp map)
+    {
+
+        
+        Queue<AudioClip> audioClips = new(table.GetOrCreateValue(map).samplesQueue);
+
+
+        float accumulatedClipLoudness = 0;
+
+
+        for (int i = 0; i < audioClips.Count; i++)
+        {
+
+            AudioClip audioClip = audioClips.Dequeue();
+            AudioSource audioSource = table.GetOrCreateValue(map).audioSource;
+
+           
+            float clipLoudness = 0f;
+            audioSource.clip = audioClip;
+            float[] sampleData = new float[audioClip.samples];
+            audioClip.GetData(sampleData, audioSource.timeSamples);
+            
+           
+            foreach(var sample in sampleData)
+            {
+                // Volume based on accumulated amplitude forced positive
+                clipLoudness += Mathf.Abs(sample);
+                
+            }
+            
+
+
+            accumulatedClipLoudness += clipLoudness / audioClip.samples;
+
+
+
+
+        }
+        table.GetOrCreateValue(map).currentAccretedAudioSampleVolume = accumulatedClipLoudness;
+
+
+
+
+
+        // += audioClip.frequency;
+        //table.GetOrCreateValue( map).currentAccretedAudioSampleFrequency += audioClip.frequency;
+    }
+
+
+
+    public static float GetAvgQueueAmplitude(this MMicrophoneFifoAmp map)
+    {
+
+        float valueout = table.GetOrCreateValue(map).currentAccretedAudioSampleVolume;
+
+
+        return valueout;
+    }
+    public static float[] GetAvgQueueFrequencyDistribution(this MMicrophoneFifoAmp map)
+    {
+
+        float[] valueout = table.GetOrCreateValue(map).frequencyBands;
+
+
+        return valueout;
+    }
+    public static void BufferFlush(this MMicrophoneFifoAmp map)
+    {
+
+        table.GetOrCreateValue(map).frequencyBands = new float[table.GetOrCreateValue(map).NumberOfFrequencyBands];
+        table.GetOrCreateValue(map).samplesQueue.Clear();        
+        table.GetOrCreateValue(map).bufferedSamples = new float[table.GetOrCreateValue(map).samplingRate];
 
     }
 
